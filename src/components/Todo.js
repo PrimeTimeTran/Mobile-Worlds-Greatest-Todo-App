65109957ef310d5be57832112cce2a45644e33bd
Loi Tran <datloiboi@gmail.com>
2019-07-14 00:10:15 +0700

    Refactor todo to functitonal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,45 +1,31 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 
-export default class Todo extends React.PureComponent {
-  toggleComplete = () => {
-    this.props.toggleComplete(this.props.id)
-  }
-  render() {
-    const isDone = this.props.status === "Done";
-    const bgStyle = isDone ? "#12355B" : "green";
+const Todo = ({ id, body, index, status, toggleComplete }) => {
+  const isDone = status === "Done";
+  const bgStyle = isDone ? "#12355B" : "green";
+  const textStyle = { textDecorationLine: isDone ? "line-through" : null };
 
-    return (
-      <TouchableHighlight
-        onPress={this.toggleComplete}
-        style={[styles.bg, { backgroundColor: bgStyle }]}
-      >
-        <View style={styles.container}>
-          <View style={{ flex: 2 }}>
-            <Text
-              style={[
-                styles.text,
-                { textDecorationLine: isDone ? "line-through" : "none" }
-              ]}
-            >
-              {this.props.index + 1}: {this.props.body}
-            </Text>
-          </View>
-          <View style={{ flex: 0.1 }}>
-            <Text
-              style={{
-                color: "white",
-                textDecorationLine: isDone ? "line-through" : null
-              }}
-            >
-              X
-            </Text>
-          </View>
+  return (
+    <TouchableHighlight
+      onPress={() => toggleComplete(id)}
+      style={[styles.bg, { backgroundColor: bgStyle }]}
+    >
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, textStyle]}>
+            {index + 1}: {body}
+          </Text>
         </View>
-      </TouchableHighlight>
-    );
-  }
-}
+        <View style={{ flex: 0.1 }}>
+          <Text style={[styles.x, textStyle]}>X</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+export default Todo;
 
 const styles = StyleSheet.create({
   bg: {
@@ -49,15 +35,21 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    padding: 10,
     margin: 10,
+    padding: 10,
     alignItems: "center",
     flexDirection: "row"
+  },
+  textContainer: { 
+    flex: 2 
   },
   text: {
     fontSize: 20,
     marginLeft: 10,
     color: "white",
     fontWeight: "bold"
+  },
+  x: {
+    color: "white"
   }
 });
